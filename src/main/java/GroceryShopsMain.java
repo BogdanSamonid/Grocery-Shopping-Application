518f561ed85dfea36e6fc2cde2346c74ec6a0ffd
Oana Tomuta <tomutaoana@gmail.com>
2020-05-14 23:57:50 +0300

    shop page added

--- src/main/java/GroceryShopsMain.java
+++ src/main/java/GroceryShopsMain.java
@@ -1,10 +1,12 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.JavaFXBuilderFactory;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class GroceryShopsMain extends Application {
@@ -26,7 +28,6 @@ public class GroceryShopsMain extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args){
         launch(args);
---
+++ src/main/java/controller/ShopPageController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ShopPageController  {
+
+    public void backToShopsList(ActionEvent event) throws IOException {
+
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/ShopListView.fxml"));
+        Scene view2 = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(view2);
+        window.show();
+    }
+
+
+}
