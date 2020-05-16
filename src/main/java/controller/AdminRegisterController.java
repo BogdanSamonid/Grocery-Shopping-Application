9a2593770a87b2824015bba050c54cc435e6b2d3
Bogdan Samonid <b.samonid@gmail.com>
2020-05-16 17:15:58 +0300

    Refined register pages and added register action handler function

--- src/main/java/controller/AdminRegisterController.java
+++ src/main/java/controller/AdminRegisterController.java
@@ -1,15 +1,31 @@
 package controller;
 
+import exceptions.EmptyIDException;
+import exceptions.EmptyPasswordException;
+import exceptions.EmptyUsernameException;
+import exceptions.UsernameAlreadyExistsException;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import service.AdminService;
 
 import java.io.IOException;
 
 public class AdminRegisterController {
+
+    @FXML
+    private TextField usernameField;
+    @FXML
+    public TextField employeeIDField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+    private TextField messageField;
 
     public void gotoUserRegister(ActionEvent event) throws IOException {
 
@@ -22,6 +38,7 @@ public class AdminRegisterController {
         window.show();
 
     }
+
     public void gotoAdminLogin(ActionEvent event) throws IOException {
 
         Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/AdminLogin.fxml"));
@@ -33,4 +50,27 @@ public class AdminRegisterController {
         window.show();
 
     }
+
+    public void registerButtonAction() {
+
+        try {
+
+            AdminService.addAdmin(usernameField.getText(), employeeIDField.getText(), passwordField.getText());
+            messageField.setText("Account created successfully!");
+        } catch (UsernameAlreadyExistsException e) {
+
+            messageField.setText(e.getMessage());
+        } catch (EmptyPasswordException passwordEmpty) {
+
+            passwordEmpty.printStackTrace();
+        } catch (EmptyIDException IDEmpty) {
+
+            IDEmpty.printStackTrace();
+        } catch (EmptyUsernameException userEmpty) {
+
+            userEmpty.printStackTrace();
+        }
+
+    }
+
 }
