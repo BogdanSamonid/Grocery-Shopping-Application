f71f4433c910dab1cc286dca85cd3d8e420f7729
Bogdan Samonid <b.samonid@gmail.com>
2020-05-16 18:52:34 +0300

    ~commit message~

--- src/main/java/controller/UserRegisterController.java
+++ src/main/java/controller/UserRegisterController.java
@@ -1,18 +1,29 @@
 package controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
 
 import java.io.IOException;
 
 
-
 public class UserRegisterController {
+
+    @FXML
+    public void gotoUserLogin(ActionEvent event) throws IOException {
+
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/UserLoginMainView.fxml"));
+        Scene view2 = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(view2);
+        window.show();
+    }
 
     @FXML
     public void gotoCustomerRegister(ActionEvent event) throws IOException {
@@ -30,19 +41,6 @@ public class UserRegisterController {
     public void gotoAdminRegister(ActionEvent event) throws IOException {
 
         Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/AdminRegister.fxml"));
-        Scene view2 = new Scene(view);
-
-        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-
-        window.setScene(view2);
-        window.show();
-
-    }
-
-    @FXML
-    public void gotoUserLogin(ActionEvent event) throws IOException {
-
-        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/UserLoginMainView.fxml"));
         Scene view2 = new Scene(view);
 
         Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
