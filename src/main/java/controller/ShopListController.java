22505644081df0fa1b993990786d8cbc1c46361e
Oana Tomuta <tomutaoana@gmail.com>
2020-05-17 19:02:36 +0300

    fxml fixes

--- src/main/java/controller/ShopListController.java
+++ src/main/java/controller/ShopListController.java
@@ -1,11 +1,16 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
@@ -51,5 +56,16 @@ public class ShopListController implements Initializable {
             b.setOnAction(new ChooseShopEventHandler());
         }
 
+    }
+    @FXML
+    public void gotoUserLogin(ActionEvent event) throws IOException {
+
+        Parent view = FXMLLoader.load(getClass().getClassLoader().getResource("view/UserLoginMainView.fxml"));
+        Scene view2 = new Scene(view);
+
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+        window.setScene(view2);
+        window.show();
     }
 }
