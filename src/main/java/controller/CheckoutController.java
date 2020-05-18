de8239463967e5d2e46d14a57d2fcd892b118829
Oana Tomuta <tomutaoana@gmail.com>
2020-05-18 17:27:48 +0300

    GSA-12 total cart value

--- src/main/java/controller/CheckoutController.java
+++ src/main/java/controller/CheckoutController.java
@@ -4,16 +4,20 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import model.Product;
 import model.Shop;
 import model.ShoppingCart;
+import org.msgpack.value.IntegerValue;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,6 +28,8 @@ public class CheckoutController<prList> {
     public TableView<ShoppingCart> table;
     public TableColumn<ShoppingCart, String> prName;
     public TableColumn<ShoppingCart, String> prPrice;
+    public TextField total;
+
 
     public void gotoShopsList(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("view/ShopsListView.fxml"));
@@ -43,8 +49,20 @@ public class CheckoutController<prList> {
         prName.setCellValueFactory(new PropertyValueFactory<>("name"));
         prPrice.setCellValueFactory(new PropertyValueFactory<>("price"));
         table.setItems(data);
+        totalPrice(table,total);
+
     }
 
+    public void totalPrice(TableView<ShoppingCart> table, TextField total ){
+        float totalSavingValue=0;
+        for (ShoppingCart value : table.getItems()) {
+            totalSavingValue = totalSavingValue + Float.parseFloat(value.getPrice());
+        }
+
+        total.setText(String.valueOf(totalSavingValue) + " ron");
+        total.setAlignment(Pos.CENTER);
+        total.setDisable(true);
+    }
 
 
 }
