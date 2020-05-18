e84be523e546aae5a3d71731ea6505e0cf270376
Oana Tomuta <tomutaoana@gmail.com>
2020-05-18 20:52:46 +0300

    refactor views

---
+++ src/main/java/controller/MainChooseShopEventHandler.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import model.Shop;
+
+import java.io.IOException;
+
+
+public class MainChooseShopEventHandler implements EventHandler<ActionEvent> {
+
+    @Override
+    public void handle(ActionEvent event) {
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("view/MainShopPageView.fxml"));
+            Parent view = loader.load();
+            Scene view2 = new Scene(view);
+
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            window.setScene(view2);
+            MainShopPageController controller = loader.getController();
+            Shop shop = new Shop();
+            shop.setShopName(((Button)event.getTarget()).getText());
+            controller.init(shop);
+
+            window.show();
+        } catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
